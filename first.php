57bb1bbd5eca220d185c15ef1a59ffe67f427db8
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-09-04 13:15:43 +0530

    add basic practice file of php

---
+++ first.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This is my first php website
+    <?php
+        // echo "</br>";
+        // echo "hello";
+        $num1=10;
+        $num2=20;
+        echo $num1+$num2;
+        echo "</br>";
+        echo "<h1>Strings</h1>";
+
+        $str="Mahesh";
+        echo $str;
+
+        echo "</br>";
+        // check datatype
+        echo var_dump($num1);
+        echo "</br>";
+        echo var_dump($str);
+        ?>
+</body>
+</html>
